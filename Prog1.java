ba343ad6b570e46b690c178ac8ce8a208f625ddc
Vergel <vergelomadto16@gmail.com>
2024-04-19 17:31:57 +0800

    added course to the code

--- Prog1.java
+++ Prog1.java
@@ -1,7 +1,9 @@
 public class Prog1 {
 	public static void main(String[] args){
-		System.out.print("PATEROS TECHNOLOGICAL COLLEGE");
-		
+		System.out.print("PATEROS");
+		System.out.print("TECHNOLOGICAL");
+		System.out.print("COLLEGE");
+		System.out.print("COURSE: BSCS");
 	}
 
 }
